5520a86d9d4e9758921014d5912f2dfa6a0f99df
fardik <nazarov_ns@sibmail.com>
2020-04-30 00:03:32 +0700

    Worked version

--- homework/settings.py
+++ homework/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'homework',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
--- homework/views.py
+++ homework/views.py
@@ -23,41 +23,120 @@ class IndexView(TemplateView):
 
         context.update(
             {
-                'students_statistics': [
-                    {
-                        'id': 1,
-                        'fio': 'Someone',
-                        'timp': 2,
-                        'eis': 3,
-                        'philosophy': 4,
-                        'english': 5,
-                        'sport': 2.3,
-                        'average': 2.3,
-                    }
-                ],
-                'excellent_students': 'Student A, Student B',
-                'bad_students': 'Student C, Student D'
+                'students_statistics': students.get_data(),
+                'excellent_students': students.get_exelent_student(),
+                'bad_students': students.get_bad_students()
             }
         )
         return context
 
-    '''
+
+'''
     def _generate_test_data(self):
         student = Student('Petr')
         return scores
-    '''
+'''
+
+
+
 
 class Student:
-    pass
+    def __init__(self, subject, data):
+        self.data_list = data
+        self.subjects = subject
+
+    def get_data(self):      # Добавляет в словарь студента среднее значение по оценкамт
+        for data_list_item in self.data_list:
+            score_values = []
+            for key in data_list_item:
+                if key in self.subjects:
+                    score_values.append(data_list_item[key])
+            average = sum(score_values)/len(score_values)
+            data_list_item['average'] = average
+        return self.data_list
+
+    def get_exelent_student(self):
+        students.get_data()
+        stat = [value['average'] for value in self.data_list]
+        exelent_average = max(stat)
+        exelent_students = [i['surname'] for i in self.data_list if i['average'] == exelent_average]
+
+        return ''.join(exelent_students)
+
+    def get_bad_students(self):
+        students.get_data()
+        bad_students = [value['surname'] for value in self.data_list if value['average'] < 3]
+
+        return ''.join(bad_students)
 
 
-class Statistics:
-    # student_id, [Scores]
-    pass
+    def add(self, data_add):
+        self.id_add = data_add['id']
+        check_data_id = True
+        for i in self.data_list:
+            if self.id_add == i['id']:
+                check_data_id = False
+        if check_data_id:
+            self.data_list.append(data_add)
 
-class Subject:
-    pass
+    def remove(self,id):
+        self.id_remove = id
+        for i in self.data_list:
+            if self.id_remove == i['id']:
+                self.data_list.remove(i)
 
-class Score:
-    # Subject, Student, value
-    pass
+
+
+
+subject = ['timp', 'eis', 'philosophy', 'english', 'sport']
+data = [
+    {
+    'id' : 1,
+    'surname' : 'Ivanov A.',
+    'timp' : 2,
+    'eis' : 5,
+    'philosophy' : 4,
+    'english' : 3,
+    'sport' : 5
+    },
+    {
+    'id' : 2,
+    'surname' : 'Sannikov S.',
+    'timp' : 4,
+    'eis' : 5,
+    'philosophy' : 3,
+    'english' : 5,
+    'sport' : 5
+    },
+{
+    'id' : 3,
+    'surname' : 'Dyatlov T.',
+    'timp' : 2,
+    'eis' : 3,
+    'philosophy' : 2,
+    'english' : 3,
+    'sport' : 4
+    }
+]
+
+students = Student(subject, data)
+
+students.add({
+    'id' : 4,
+    'surname' : 'Petrov E.',
+    'timp' : 2,
+    'eis' : 5,
+    'philosophy' : 4,
+    'english' : 2,
+    'sport' : 4
+    })
+
+# students.remove(2)
+
+
+
+
+
+
+
+
